625dfcf2fb67d65a1da01849002cc054353d02a5
Gabriel García Seco <gabriel@streamloots.com>
2019-12-29 00:31:41 +0100

    leave weight behind

---
+++ src/ReactTranslate.stories.tsx
@@ -0,0 +1,114 @@
+import React from 'react';
+
+import {
+  TranslateProvider,
+  TranslateContext,
+  Trans,
+  useTranslate
+} from './index';
+
+export default {
+  title: 'ReactTranslate',
+  component: TranslateProvider
+};
+
+const Children = () => {
+  const { t } = useTranslate('common', 'dashboard');
+  const [count, setCount] = React.useState(0);
+  const context = React.useContext(TranslateContext);
+
+  if (!context) {
+    return null;
+  }
+
+  const { lang, switchLanguage } = context;
+
+  return (
+    <>
+      <pre>{lang}</pre>
+      <pre>{t('dashboard:broncano.say.hi')}</pre>
+      <pre>{t('common:hello-world')}</pre>
+      <pre>
+        {t('common:hello-world-with-interpolations', {
+          value: 'Gabriel'
+        })}
+      </pre>
+      <pre>
+        <Trans translation={t('common:hard-interpolation')}>
+          This is a fallback in case that <strong>the component</strong> does
+          not load correctly
+        </Trans>
+      </pre>
+
+      <pre>
+        <Trans
+          translation={t('common:hard-interpolation-with-props', {
+            count: count
+          })}
+        >
+          This is my count <strong>{count}</strong>
+        </Trans>
+      </pre>
+      <button onClick={() => setCount(count => count + 1)}>
+        increment count
+      </button>
+      <button onClick={() => switchLanguage('es')}>
+        cambiar lenguage a es
+      </button>
+      <button onClick={() => switchLanguage('en')}>
+        cambiar lenguage a en
+      </button>
+    </>
+  );
+};
+
+const commonEN = {
+  'hello-world': 'hello world',
+  'hello-world-with-interpolations': 'hello world my name is {{value}}',
+  'hard-interpolation':
+    'This is a fallback in case that <strong>the component</strong> does not load correctly',
+  'hard-interpolation-with-props': 'This is my count <strong>{{count}}</strong>'
+};
+const commonES = {
+  'hello-world': 'hola mundo',
+  'hello-world-with-interpolations': 'hola mundo mi nombre es {{value}}',
+  'hard-interpolation':
+    'Esto es un fallback en caso de que <strong>el componente</strong> no se cargue bien',
+  'hard-interpolation-with-props': 'Mi cuenta es <strong>{{count}}</strong>'
+};
+const dashboardEN = {
+  broncano: {
+    say: {
+      hi: 'Oh Mum'
+    }
+  }
+};
+const dashboardES = {
+  broncano: {
+    say: {
+      hi: 'Oh Mama'
+    }
+  }
+};
+
+export const TranslateDemo = () => {
+  const providerValue = {
+    fallbackLng: 'en',
+    languages: ['en', 'es'],
+    translations: {
+      en: {
+        common: commonEN,
+        dashboard: dashboardEN
+      },
+      es: {
+        common: commonES,
+        dashboard: dashboardES
+      }
+    }
+  };
+  return (
+    <TranslateProvider value={providerValue}>
+      <Children />
+    </TranslateProvider>
+  );
+};
--- src/useTranslate.tsx
+++ src/useTranslate.tsx
@@ -1,15 +1,31 @@
 import React from 'react';
-import Mustache from 'mustache';
 import warning from 'warning';
 
 import { TranslateContext } from './TranslateContext';
+
+const isNil = (value: unknown) => {
+  return value === null || value === undefined;
+};
+
+const replaceBrackets = (
+  translationValue: string,
+  options?: { [key: string]: any }
+): string => {
+  return translationValue.replace(/{{(.+?)}}/g, (_, g1) => {
+    return options && !isNil(options) ? options[g1] : g1;
+  });
+};
 
 const useTranslate = (...args: string[]) => {
   const context = React.useContext(TranslateContext);
 
   if (context === null) {
     console.error('Context is not defined, you should define it');
-    return;
+    return {
+      t: (key: string, options?: { [key: string]: any }): string => {
+        return '';
+      }
+    };
   }
 
   const { translations, lang } = context;
@@ -34,7 +50,7 @@ const useTranslate = (...args: string[]) => {
     const regex = new RegExp(/{{{?(#[a-z]+ )?[a-z]+.[a-z]*}?}}/);
 
     const parsedValue = regex.test(translationValue)
-      ? Mustache.render(translationValue, options)
+      ? replaceBrackets(translationValue, options)
       : translationValue;
     return parsedValue;
   };
