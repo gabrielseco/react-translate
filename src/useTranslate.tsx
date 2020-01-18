d81471062ec8f6bd979b50275928f0b27ab192dd
Gabriel García Seco <ggarciaseco@gmail.com>
2020-01-19 00:35:30 +0100

    plurals (#4)

--- src/useTranslate.tsx
+++ src/useTranslate.tsx
@@ -3,10 +3,9 @@ import React from 'react';
 import { isDev } from './utils';
 import { TranslateContext } from './TranslateContext';
 
-export type tFunction = (
-  key: string,
-  options?: { [key: string]: any }
-) => string;
+type Options = { [key: string]: any };
+
+export type tFunction = (key: string, options?: Options) => string;
 
 const isNil = (value: unknown): boolean => {
   return value === null || value === undefined;
@@ -14,21 +13,63 @@ const isNil = (value: unknown): boolean => {
 
 const replaceBrackets = (
   translationValue: string,
-  options?: { [key: string]: any }
+  options?: Options
 ): string => {
   return translationValue.replace(/{{(.+?)}}/g, (_, g1) => {
     return options && !isNil(options) ? options[g1] : g1;
   });
 };
 
+const hasPlurals = (options: Options): boolean => {
+  const arrValueOptions = Object.keys(options)
+    .map(key => {
+      return {
+        type: typeof options[key],
+        value: options[key]
+      };
+    })
+    .filter(value => value.type === 'number' && value.value > 1);
+
+  return arrValueOptions.length > 0;
+};
+
+const searchValueInMemory = ({
+  jsonKey,
+  searchSeparator,
+  lang,
+  namespace,
+  translations
+}: {
+  jsonKey: string;
+  searchSeparator: string;
+  lang: string;
+  namespace: string;
+  translations: Record<string, any>;
+}): string | undefined => {
+  let translationValue: string;
+
+  if (jsonKey.indexOf(searchSeparator) !== -1) {
+    translationValue = jsonKey
+      .split('.')
+      .reduce((o, i) => o[i], translations[lang][namespace]);
+  } else {
+    translationValue = translations[lang][namespace][jsonKey];
+  }
+
+  return translationValue;
+};
+
 const useTranslate = (): { t: tFunction } => {
   const __DEV__ = isDev();
+  const namespaceSeparator = ':';
+  const searchSeparator = '.';
+  const pluralsSuffix = '_plural';
   const context = React.useContext(TranslateContext);
 
   if (context === null) {
     console.error('Context is not defined, you should define it');
     return {
-      t: (key: string, options?: { [key: string]: any }): string => {
+      t: (key: string, options?: Options): string => {
         return '';
       }
     };
@@ -36,25 +77,47 @@ const useTranslate = (): { t: tFunction } => {
 
   const { translations, lang } = context;
 
-  const t = (key: string, options?: { [key: string]: any }): string => {
-    const namespaceSeparator = ':';
-    const searchSeparator = '.';
+  const t = (key: string, options?: Options): string => {
     const hasNamespace = key.includes(namespaceSeparator);
+    const optionsHasPlurals = options ? hasPlurals(options) : false;
+
     if (!hasNamespace) {
       if (__DEV__) {
         throw new Error(`${key} you passed should have a namespace`);
       }
       return '';
     }
-    const [namespace, jsonKey] = key.split(namespaceSeparator);
-    let translationValue: string;
 
-    if (jsonKey.indexOf(searchSeparator) !== -1) {
-      translationValue = jsonKey
-        .split('.')
-        .reduce((o, i) => o[i], translations[lang][namespace]);
+    const [namespace, jsonKey] = key.split(namespaceSeparator);
+
+    let translationValue: string | undefined;
+
+    if (optionsHasPlurals) {
+      const temporalPluralValue = searchValueInMemory({
+        jsonKey: jsonKey + pluralsSuffix,
+        namespace,
+        translations,
+        lang,
+        searchSeparator
+      });
+      translationValue =
+        temporalPluralValue !== undefined
+          ? temporalPluralValue
+          : searchValueInMemory({
+              jsonKey,
+              namespace,
+              translations,
+              lang,
+              searchSeparator
+            });
     } else {
-      translationValue = translations[lang][namespace][jsonKey];
+      translationValue = searchValueInMemory({
+        jsonKey,
+        namespace,
+        translations,
+        lang,
+        searchSeparator
+      });
     }
 
     if (translationValue === undefined) {
