fb42489b02c71a2dbc82b5dca551b3298132f581
Gabriel García Seco <ggarciaseco@gmail.com>
2020-08-21 18:24:25 +0200

    Feature/fix hoc (#60)
    * update dependencies
    
    * Change naming

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -1,6 +1,4 @@
-import { configure, addDecorator, addParameters } from '@storybook/react';
-import { withKnobs } from '@storybook/addon-knobs';
-import { INITIAL_VIEWPORTS } from '@storybook/addon-viewport';
+import { addParameters } from '@storybook/react';
 
 addParameters({
   options: {
@@ -8,11 +6,5 @@ addParameters({
       a[1].kind === b[1].kind
         ? 0
         : a[1].id.localeCompare(b[1].id, { numeric: true })
-  },
-  viewport: {
-    viewports: INITIAL_VIEWPORTS
   }
 });
-addDecorator(withKnobs);
-
-configure([require.context('../src', true, /\.stories\.tsx$/)], module);
